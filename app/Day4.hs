29f2245535bfc8743ebe57bd56cbc60f266c84dc
Lucas Borjesson <lucas.borjesson@zimpler.com>
2023-12-05 07:39:39 +0100

    Day 4 part 2

--- app/Day4.hs
+++ app/Day4.hs
@@ -8,7 +8,7 @@ import Data.List.Split (wordsBy)
 import InputData (getInputData)
 import Data.List (intersect, nub)
 
-data Card = Card{id :: Int, winVals :: [Int], vals :: [Int]} deriving (Show, Eq)
+data Card = Card{idNo :: Int, winVals :: [Int], vals :: [Int]} deriving (Show, Eq)
 
 filterDigits :: String -> String
 filterDigits val = filter isDigit val
@@ -29,12 +29,27 @@ winningNumbers c = nub (intersect (winVals c) (vals c))
 calcPoints :: [Int] -> Int
 calcPoints l = if length l == 0 then 0 else 2^((length l) - 1)
 
+getCard :: Int -> Card -> Bool
+getCard i card = (idNo card) == i
+
+newCards :: [Card] -> Card -> [Card]
+newCards cards c = do
+  let wins = length (winningNumbers c)
+  concatMap (\cId -> filter (getCard cId) cards) [((idNo c)+1)..((idNo c)+wins)]
+
+newCards1 :: [Card] -> [Card] -> [Card]
+newCards1 orgcards [] = []
+newCards1 orgcards (card:rest) = card : newCards1 orgcards ((newCards orgcards card) ++ rest)
+
 day4 :: IO()
 day4 = do
   respb <- getInputData 4
+  let cards = (map parseCards (lines respb))
   putStrLn "--- PART 1 -------------------------"
-  print (sum (map calcPoints (map winningNumbers (map parseCards (lines respb)))))
+  print (sum (map calcPoints (map winningNumbers cards)))
   putStrLn "------------------------------------"
 
   putStrLn "--- PART 2 -------------------------"
+  let nnCards = newCards1 cards cards
+  print (length nnCards)
   putStrLn "------------------------------------"
